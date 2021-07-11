c0abb2e059db8c0b383f78651dbc929a1905dbb0
RikoBato <s20c1101jd@s.chibakoudai.jp>
2021-07-11 22:23:58 +0900

    7/11

--- kadai2.cpp
+++ kadai2.cpp
@@ -1,1 +1,20 @@
 #include<iostream>
+#include<string>
+int main(){
+  //std::string  c(1,0);
+  int c=0;
+  std::string N(20001,0);
+  std::cin>>N;
+  //c=str[2]-'0';
+ //std::cout<<c<<std::endl;
+  for(unsigned int i= 0; i<N.size();i++){
+    c+=int (N[i]-'0');
+  }
+  std::cout<<c<<std::endl;
+  if(c%9==0){
+    std::cout<<"Yes"<<std::endl;
+  }else{
+    std::cout<<"No"<<std::endl;
+  }
+  return 0;
+}

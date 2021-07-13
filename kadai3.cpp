9bab8511bd502dc4a24545c1d29085b237196d75
RikoBato <s20c1101jd@s.chibakoudai.jp>
2021-07-13 10:42:15 +0900

    clear

--- kadai3.cpp
+++ kadai3.cpp
@@ -2,14 +2,15 @@
 #include<vector>
 
 int main(){
-  int N,A,B;
+  unsigned long  N,A,B;
   long int num=0;
   std::cin>>N;
-    for(int i=0;i<N;i++){
+    for(unsigned long i=0;i<N;i++){
       std::cin>>A>>B;
-      for(;A<B+1;A++){
+      num+=(B*(B+1)-A*(A-1))/2;
+           /* for(;A<B+1;A++){
         num+=A;
-      }
+      }*/
     }
   std::cout<<num<<std::endl;
   return 0;
